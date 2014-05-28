b63d1dcba43ae2149a65d58f314d89777383f3af
Tony Pelaez <tnyplz@gmail.com>
2014-05-28 04:58:53 +0000

    refactor temp calculation

--- temp.js
+++ temp.js
@@ -20,22 +20,26 @@ function resistance_to_fahrenheight(x) {
     return f;
 }
 
+function roundTemp(x) {
+    return Math.round(resistance_to_fahrenheight(x) * 10)/10;
+}
+
 function printStatus(x) {
     console.log('Analog Value: ' + x);
     console.log('Resistance: ' + calculate_resistance(x));
-    console.log('Temp: ' + Math.round(resistance_to_fahrenheight(x) * 10)/10 + ' °F');
+    console.log('Temp: ' + roundTemp(x) + ' °F');
 }
 
 function get_temp(){
   var sample = [];
+  var total = 0;
   for (let i=0;i<5;i++) {
       b.analogRead('P9_40', function(x){
           sample.push(x.value);
           if (sample.length == 5) {
-	    var total = 0;
-	    for (let s=0;s<5;s++) {total += sample[s];}
-	    printStatus(total/sample.length);
-	  }
+	            for (let s=0;s<5;s++) {total += sample[s];}
+	            printStatus(total/sample.length);
+	        }
       });
   }
 }
