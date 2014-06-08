0712d46ba3e2b838bc2225a97257a07885637e2d
Tony Pelaez <tnyplz@gmail.com>
2014-06-08 04:37:02 +0000

    setup flot

--- public/javascripts/site.js
+++ public/javascripts/site.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
   checkTemp();
+  var data = [[-373597200000, 315.71], [-370918800000, 317.45], [-368326800000, 317.50], [-363056400000, 315.86], [-360378000000, 314.93]];
+  $.plot($("#placeholder"), [data], {
+    xaxis: {
+      mode: "time",
+      timezone: "browser"
+    }
+  });
 });
 
 function checkTemp() {
