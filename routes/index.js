a6d6ef82ff371b1e0bf9beb225ff79b5079282f0
Tony Pelaez <tnyplz@gmail.com>
2014-06-08 01:03:45 +0000

    refactor

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,10 @@
 var express = require('express'),
     router = express.Router(),
-    temp = require('../lib/temp'),
-    current_temp = 0;
+    temp = require('../lib/temp');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  temp.current(function(t){
-    console.log('new temp: ' + t);
-    current_temp = t;
+  temp.current(function(current_temp){
     res.render('index', { title: 'TempContoller', temp: current_temp });
   });
 });
