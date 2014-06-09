39e6673d24864b68487af1e184db62da960a07a9
Tony Pelaez <tnyplz@gmail.com>
2014-06-09 05:31:32 +0000

    add button to clear graph

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,18 @@
 var express = require('express'),
     router = express.Router(),
-    temp = require('../lib/temp');
+    temp = require('../lib/temp'),
+    data = require('../lib/data');
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'TempContoller', temp: '' });
+});
+
+router.post('/', function(req, res) {
+  if (req.body.edit == "Clear") {
+    data.clear('batch');
+  }
+  res.redirect('back');
 });
 
 module.exports = router;
