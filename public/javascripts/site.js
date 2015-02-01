0b11cab27d0baaa64c74d013f9018d5f2689a333
Tony Pelaez <tnyplz@gmail.com>
2015-02-01 08:33:32 +0000

    add buttons to change target temp

--- public/javascripts/site.js
+++ public/javascripts/site.js
@@ -7,6 +7,33 @@ $(document).ready(function(){
   });
 
   socket.on('update chart', getChartData());
+
+  $('#target').addClass('col-xs-8').css('text-align', 'right')
+              .after('<div class="col-xs-4 buttons"></div>');
+  $('.buttons').append('<div class="row"><a href="#" class="up"><img src="images/up.svg" alt="up" /></a></div>').css('margin-top', '0.7em')
+               .append('<div class="row" style="margin-top: 0.3em;"><a href="#" class="dn"><img src="images/down.svg" alt="down" /></a></div>');
+  $('#target-form').hide();
+  var temp_change_timer = null;
+  $('.up').click(function(){
+    var temp = parseInt($('.tmp-target').val());
+    if (temp < 99) temp += 1;
+    $('.tmp-target').val(temp);
+    $('#target.temp').text(temp);
+    clearTimeout(temp_change_timer);
+    temp_change_timer = setTimeout(function(){
+      $('#target-form').submit();
+    }, 10000);
+  });
+  $('.dn').click(function(){
+    var temp = parseInt($('.tmp-target').val());
+    if (temp > 35) temp -= 1;
+    $('.tmp-target').val(temp);
+    $('#target.temp').text(temp);
+    clearTimeout(temp_change_timer);
+    temp_change_timer = setTimeout(function(){
+      $('#target-form').submit();
+    }, 10000);
+  });
 });
 
 function getChartData() {
